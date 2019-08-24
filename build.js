76d04c109a33260ba5b65d33fbf00c67f7dce571
Gürkan Biçer <gurkan@grkn.co>
2019-08-24 16:11:51 +0300

    build.js file created & prepared

---
+++ build.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const axios = require('axios');
+
+axios({
+  url: 'http://localhost:' + process.env.PORT + '/build-all',
+  method: 'GET',
+}).then(response => {
+  console.log(response.data);
+}).catch(error => {
+  console.log(error);
+});
