bb2abbd3fa2062819207edb33f37bae78be67ed0
Gürkan Biçer <gurkan@grkn.co>
2019-08-24 16:10:36 +0300

    index.js file created & prepared

---
+++ routes/index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const axios = require('axios');
+const fs = require('fs-extra');
+const router = express.Router();
+const listEndpoints = require('express-list-endpoints');
+
+function writeOutputToDist(fileName, output) {
+  try {
+    fs.ensureDirSync(process.env.DIST_DIR);
+    if (fileName === "/") fileName = '/index';
+    fs.outputFileSync(process.env.DIST_DIR + fileName + '.html', output);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+router.get('/', function (req, res) {
+  const params = {
+    title: 'Page: /'
+  };
+  res.render('index', params, function (err, output) {
+    writeOutputToDist(req.path, output);
+    res.send(output);
+  });
+});
+
+router.get('/another-sample', function (req, res) {
+  const params = {
+    title: 'Page: /another-sample'
+  };
+  res.render('index', params, function (err, output) {
+    writeOutputToDist(req.path, output);
+    res.send(output);
+  });
+});
+
+router.get('/build-all', async function (req, res) {
+  try {
+    const routes = listEndpoints(router);
+    let successfulPaths = [];
+    let failedPaths = [];
+
+    fs.emptyDirSync(process.env.DIST_DIR);
+    fs.ensureDirSync(process.env.DIST_DIR + '/assets');
+    fs.copySync('public/assets/fonts', process.env.DIST_DIR + '/assets/fonts');
+    fs.copySync('public/assets/images', process.env.DIST_DIR + '/assets/images');
+    fs.copySync('public/assets/javascripts', process.env.DIST_DIR + '/assets/javascripts');
+    fs.copySync('public/assets/stylesheets', process.env.DIST_DIR + '/assets/stylesheets');
+
+    for (const route of routes) {
+      if (route.path !== '/build-all') {
+        await axios({
+          url: 'http://localhost:' + process.env.PORT + route.path,
+          method: 'GET',
+          timeout: 1000,
+        }).then(response => {
+          successfulPaths.push(route.path);
+        }).catch(error => {
+          failedPaths.push(route.path);
+          console.log(error);
+        });
+      } else {
+        continue;
+      }
+    }
+
+    res.json({
+      'successfulPaths' : successfulPaths,
+      'failedPaths' : failedPaths
+    });
+  } catch (e) {
+    res.json(e);
+  }
+});
+
+module.exports = router;
